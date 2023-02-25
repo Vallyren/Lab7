866b4558e3a2efc338a6f19242179072bf765ac9
Vallyren <122459067+Vallyren@users.noreply.github.com>
2023-02-25 19:03:33 +0300

    Add files via upload

---
+++ packet_sniff-219701-6e7d3e.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import time
+import scapy.all as scapy
+from scapy.layers import http
+
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+
+def get_url (packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        url = get_url(packet)
+        print("[+] HTTP Request >>" + url.decode())
+
+sniff("eth0")                                                                                                                                                                                                                                                                                                                                                                     
